ffe580c230e6b8fa53058050de57335c719b10f2
Paige Ruten <paige.ruten@gmail.com>
2021-12-12 02:45:45 -0600

    Sound accessors

--- bloops/bloops.c
+++ bloops/bloops.c
@@ -180,6 +180,103 @@ static int l_bloops_sound_meta_tostring(lua_State *L) {
   return 1;
 }
 
+static float *lookup_sound_attr(bloopsaphone *sound, const char *attr) {
+  if (!strcmp(attr, "arp")) return &sound->params.arp;
+  if (!strcmp(attr, "aspeed")) return &sound->params.aspeed;
+  if (!strcmp(attr, "attack")) return &sound->params.attack;
+  if (!strcmp(attr, "decay")) return &sound->params.decay;
+  if (!strcmp(attr, "dslide")) return &sound->params.dslide;
+  if (!strcmp(attr, "freq")) return &sound->params.freq;
+  if (!strcmp(attr, "hpf")) return &sound->params.hpf;
+  if (!strcmp(attr, "hsweep")) return &sound->params.hsweep;
+  if (!strcmp(attr, "limit")) return &sound->params.limit;
+  if (!strcmp(attr, "lpf")) return &sound->params.lpf;
+  if (!strcmp(attr, "lsweep")) return &sound->params.lsweep;
+  if (!strcmp(attr, "phase")) return &sound->params.phase;
+  if (!strcmp(attr, "psweep")) return &sound->params.psweep;
+  // `repeat` is a Lua keyword, so you can't use it in the dot syntax `sound.repeat = 0.123`.
+  // 2 workarounds are provided:
+  //   a) Use the `rep` alias: `sound.rep = 0.123`
+  //   b) Use ugly square brackets: `sound["repeat"] = 0.123`
+  if (!strcmp(attr, "rep")) return &sound->params.repeat;
+  if (!strcmp(attr, "repeat")) return &sound->params.repeat;
+  if (!strcmp(attr, "resonance")) return &sound->params.resonance;
+  if (!strcmp(attr, "slide")) return &sound->params.slide;
+  if (!strcmp(attr, "square")) return &sound->params.square;
+  if (!strcmp(attr, "sustain")) return &sound->params.sustain;
+  if (!strcmp(attr, "sweep")) return &sound->params.sweep;
+  if (!strcmp(attr, "punch")) return &sound->params.punch;
+  if (!strcmp(attr, "vibe")) return &sound->params.vibe;
+  if (!strcmp(attr, "vspeed")) return &sound->params.vspeed;
+  if (!strcmp(attr, "vdelay")) return &sound->params.vdelay;
+  if (!strcmp(attr, "volume")) return &sound->params.volume;
+
+  return NULL;
+}
+
+static int l_bloops_sound_meta_index(lua_State *L) {
+  luaL_checktype(L, 1, LUA_TUSERDATA);
+  lua_getiuservalue(L, 1, 1);
+  luaL_argcheck(L, lua_islightuserdata(L, -1), 1, "not a valid Bloops.Sound instance");
+  bloopsaphone *sound = lua_touserdata(L, -1);
+
+  if (lua_isstring(L, 2)) {
+    lua_pushvalue(L, 2);
+    const char *key = lua_tostring(L, -1);
+
+    if (!strcmp(key, "type")) {
+      lua_pushinteger(L, sound->params.type);
+      return 1;
+    }
+
+    float *sound_attr = lookup_sound_attr(sound, key);
+    if (sound_attr != NULL) {
+      lua_pushnumber(L, *sound_attr);
+      return 1;
+    }
+  }
+
+  if (!lua_getmetatable(L, 1)) {
+    return luaL_argerror(L, 1, "not a valid Bloops.Sound instance");
+  }
+
+  lua_pushvalue(L, 2);
+  lua_rawget(L, -2);
+  return 1;
+}
+
+static int l_bloops_sound_meta_newindex(lua_State *L) {
+  luaL_checktype(L, 1, LUA_TUSERDATA);
+  lua_getiuservalue(L, 1, 1);
+  luaL_argcheck(L, lua_islightuserdata(L, -1), 1, "not a valid Bloops.Sound instance");
+  bloopsaphone *sound = lua_touserdata(L, -1);
+
+  if (lua_isstring(L, 2)) {
+    lua_pushvalue(L, 2);
+    const char *key = lua_tostring(L, -1);
+
+    if (!strcmp(key, "type")) {
+      sound->params.type = luaL_checkinteger(L, 3);
+      return 0;
+    }
+
+    float *sound_attr = lookup_sound_attr(sound, key);
+    if (sound_attr != NULL) {
+      *sound_attr = luaL_checknumber(L, 3);
+      return 0;
+    }
+  }
+
+  if (!lua_getmetatable(L, 1)) {
+    return luaL_argerror(L, 1, "not a valid Bloops.Sound instance");
+  }
+
+  lua_pushvalue(L, 2);
+  lua_pushvalue(L, 3);
+  lua_rawset(L, -3);
+  return 0;
+}
+
 static int l_bloops_track_meta_tostring(lua_State *L) {
   luaL_checktype(L, 1, LUA_TUSERDATA);
   lua_getiuservalue(L, 1, 1);
@@ -209,6 +306,8 @@ static const luaL_Reg bloops_methods[] = {
 static const luaL_Reg bloops_sound_methods[] = {
   {"test", l_bloops_sound_test},
   {"__tostring", l_bloops_sound_meta_tostring},
+  {"__index", l_bloops_sound_meta_index},
+  {"__newindex", l_bloops_sound_meta_newindex},
   {NULL, NULL}
 };
 
@@ -230,8 +329,6 @@ extern int luaopen_bloops(lua_State *L) {
   lua_setfield(L, -2, "NOISE");
 
   luaL_newlib(L, bloops_sound_methods);
-  lua_pushvalue(L, -1);
-  lua_setfield(L, -2, "__index");
   lua_setfield(L, -2, "Sound");
 
   luaL_newlib(L, bloops_track_methods);
--- bloops/test.lua
+++ bloops/test.lua
@@ -11,3 +11,25 @@ b:tune(saw, "c5 c6 b4 b5 d5 d6 e5 e6")
 
 b:play()
 while not b:is_stopped() do b:sleep(1) end
+
+beat = b:sound(Bloops.NOISE)
+beat["repeat"] = 0.6
+beat2 = b:sound(Bloops.NOISE)
+beat2["repeat"] = 0.2
+beat3 = b:sound(Bloops.SQUARE)
+beat3.sustain = 0.25
+beat3.decay = 0.2
+beat3.slide = 0.2
+beat3.square = 0.3
+beat3.vibe = 0.25
+beat3.vspeed = 0.25
+
+b:tune(beat, "4 4 4 b4 4 d5 4 e5")
+b:tune(beat2, "c2 4 c2 4 c2 4 c2 4")
+b:tune(beat3, "4 4 4 4 4 c2 c5 4")
+
+while true do
+  b:play()
+  saw:test()
+  while not b:is_stopped() do b:sleep(0.02) end
+end
